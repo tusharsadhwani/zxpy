b0ad5f124f2e8fc4ff521eda26d0a71d019d2501
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-05-08 16:32:29 +0530

    Use ast properly this time

--- zx.py
+++ zx.py
@@ -1,41 +1,77 @@
 """zxpy: Shell scripts made simple"""
 import ast
-import fileinput
-import shlex
 import subprocess
+import sys
+
+import astpretty
 
 
 def cli() -> None:
-    for line in fileinput.input():
-        if line.lstrip().startswith('#'):
-            continue
+    filenames = sys.argv[1:]
+    for filename in filenames:
+        with open(filename) as file:
+            module = ast.parse(file.read())
+            run_zxpy(filename, module)
 
-        if len(line.strip()) == 0:
-            continue
 
-        line_ast = ast.parse(line)
-        statement = line_ast.body[0]
-        if not isinstance(statement, (ast.Expr, ast.Assign)):
-            print(f'Syntax curently unsupported: {line}')
-            return
+def run_shell(command: str) -> str:
+    output = subprocess.getoutput(command)
+    return output
 
-        expr = statement.value
-        if (
-            isinstance(expr, ast.UnaryOp)
-            and isinstance(expr.op, ast.Invert)
-            and isinstance(expr.operand, ast.Str)
-        ):
-            shell_command = expr.operand.value
-            output = subprocess.getoutput(shell_command)
 
-            if isinstance(statement, ast.Assign):
-                variable = statement.targets[0]
-                assert isinstance(variable, ast.Name)
-                py_command = f'{variable.id} = {output}'
-                exec(py_command)
+class BashCaller(ast.NodeTransformer):
+    @staticmethod
+    def modify_expr(expr: ast.Expr) -> ast.Expr:
+        if not isinstance(expr.value, ast.UnaryOp):
+            return expr
 
-            else:
-                print(output)
+        if not isinstance(expr.value.op, ast.Invert):
+            return expr
 
-        else:
-            exec(line)
+        if not isinstance(expr.value.operand, ast.Str):
+            return expr
+
+        expr.value = ast.Call(
+            func=ast.Name(id='run_shell', ctx=ast.Load()),
+            args=[expr.value.operand],
+            keywords=[]
+        )
+        return expr
+
+    def visit_Expr(self, expr: ast.Expr) -> ast.Expr:
+        return self.modify_expr(expr)
+
+    def visit_Assign(self, assign: ast.Assign) -> ast.Assign:
+        if isinstance(assign.value, ast.expr):
+            expr = ast.Expr(assign.value)
+            new_expr = self.modify_expr(expr)
+            assign.value = new_expr.value
+        return assign
+
+
+def run_zxpy(filename: str, module: ast.Module) -> None:
+    transformer = BashCaller()
+    transformer.visit(module)
+
+    for statement in module.body:
+        if isinstance(statement, ast.Expr):
+            expr = statement.value
+            if not isinstance(expr, ast.Call):
+                continue
+
+            if not isinstance(expr.func, ast.Name):
+                continue
+
+            if not expr.func.id == 'run_shell':
+                continue
+
+            new_expr = ast.Call(
+                func=ast.Name(id='print', ctx=ast.Load()),
+                args=[expr],
+                keywords=[],
+            )
+            statement.value = new_expr
+
+    ast.fix_missing_locations(module)
+
+    exec(compile(module, filename, mode='exec'))
