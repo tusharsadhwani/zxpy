5b0db6b7739b683ade721fa3f1ec555b1769eb03
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2023-02-21 13:23:23 +0530

    Make CLI arguments accessible in zxpy programs (#52)
    * Fix sys.argv passed to tests
    
    * Improve test assertion
    
    * Add $ arg support
    
    * Normalize /bin/sh as first argument of script_args
    
    * Add type def

--- tests/test_files/argv.py
+++ tests/test_files/argv.py
@@ -1,4 +1,8 @@
 import sys
 
-assert len(sys.argv) == 1
-assert sys.argv[0].endswith("argv.py")
+assert len(sys.argv) == 3
+assert sys.argv[1] == "foobar"
+assert sys.argv[2] == "baz"
+
+out = ~"echo $1 and $2"
+assert out == "foobar and baz\n"
